3ded0a5ad9677ec1e4acad84aee184293d94b953
EdgarsSalmins <edgars.salmins@gmail.com>
2019-08-01 22:52:25 +0300

    Basics

---
+++ Basics/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    //todo: fix the code
+    class Program
+    {
+        static string Hello => "";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(); //Expected result "Hello World"
+            Console.ReadKey();
+        }
+    }
+}
