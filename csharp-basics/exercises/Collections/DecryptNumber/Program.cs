e8451d4d677da46264efba5f856a4fa468de73e3
EdgarsSalmins <edgars.salmins@gmail.com>
2023-04-06 23:03:36 +0300

    minor hint

--- csharp-basics/exercises/Collections/DecryptNumber/Program.cs
+++ csharp-basics/exercises/Collections/DecryptNumber/Program.cs
@@ -5,6 +5,7 @@ namespace DecryptNumber
 {
     internal class Program
     {
+        // look at the keyboard.
         static void Main(string[] args)
         {
             var cryptedNumbers = new List<string>
