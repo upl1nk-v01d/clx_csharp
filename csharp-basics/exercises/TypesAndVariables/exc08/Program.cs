3c72bcfcd1ee007ee35a8372aeba49c5cdae9fc7
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-16 14:19:02 +0300

    excercise 08 done

---
+++ csharp-basics/exercises/TypesAndVariables/exc08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        //there was error with finding project file, just created new c# console app.
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("ievadi minutes");
+            var minutes = int.Parse(Console.ReadLine());
+
+            decimal minutesInYear = 365 * 24 * 60;
+            decimal miutesInDay = 24 * 60;
+
+            decimal years = minutes / minutesInYear;
+            decimal minutesToDays = minutes % minutesInYear;
+            decimal days = minutesToDays / miutesInDay;
+            
+            //just needed a little text formatting
+            Console.WriteLine($"it's {years:0.000} years and {days:0.000} days");
+
+            Console.ReadKey();
+        }
+    }
+}
