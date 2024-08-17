dbbe3a15d67df4e272c01945e2b764d229dd82c8
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-17 11:36:26 +0300

    exercise 03 is done

--- csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
+++ csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
@@ -1,18 +1,29 @@
-﻿namespace SumAverageRunningInt
+﻿using System;
+
+namespace SumAverageRunningInt
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var sum = 0;
-            int average;
+            decimal sum = 0m; //redefined vartype from in to decimal
+            decimal average; //redefined vartype from in to decimal
+            int c = 0; //iterations counter for average calculations
             const int lowerBound = 1;
             const int upperBound = 100;
 
             for (var number = lowerBound; number <= upperBound; ++number)
             {
-                sum += number;
+                sum += number; c++;
             }
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine($"The sum of {lowerBound} to {upperBound} is {sum}");
+
+            average = sum / c; //average calculation
+
+            Console.WriteLine($"The average is {average:0.0}");
+            Console.WriteLine();
         }
     }
 }
