f50858c41348d6baf5c3de08eee304cfff436e2d
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-17 09:39:06 +0300

    changed variable names to camel case

--- csharp-basics/exercises/TypesAndVariables/MoreVariablesAndPrinting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/MoreVariablesAndPrinting/Program.cs
@@ -8,28 +8,31 @@ namespace MoreVariablesAndPrinting
         {
             string name = "Zed A. Shaw";
             int age = 35;
-            int h_inches = 74;  // height of inches
-            int w_pounds = 180; // weight of pounds (lbs)
+            int heightInches = 74;  // height of inches
+            int weightPounds = 180; // weight of pounds (lbs)
             string eyes = "Blue";
             string teeth = "White";
             string hair = "Brown";
-            decimal k_cms = 2.54m; //coefficient of centimeters
-            decimal k_kgs = 0.453592m; //coefficient of kilograms
+            decimal coefCentimeters = 2.54m; //coefficient of centimeters
+            decimal coefKilograms = 0.453592m; //coefficient of kilograms
 
-            decimal h_cms = Convert.ToDecimal(h_inches) * k_cms; //convertion to centimeters
-            decimal w_kgs = Convert.ToDecimal(w_pounds) * k_kgs; //convertion to kilograms
+            //convertion to centimeters
+            decimal heightCentimeters = Convert.ToDecimal(heightInches) * coefCentimeters; 
+
+            //convertion to kilograms
+            decimal weightKilograms = Convert.ToDecimal(weightPounds) * coefKilograms; 
 
             Console.WriteLine("Let's talk about " + name + ".");
-            Console.WriteLine("He's " + String.Format("{0:0.0#}",h_cms) + " centimeters tall.");
-            Console.WriteLine("He's " + String.Format("{0:0.0#}",w_kgs) + " kilograms heavy.");
+            Console.WriteLine("He's " + String.Format("{0:0.0#}",heightCentimeters) + " centimeters tall.");
+            Console.WriteLine("He's " + String.Format("{0:0.0#}",weightKilograms) + " kilograms heavy.");
             Console.WriteLine("Actually, that's not too heavy.");
             Console.WriteLine("He's got " + eyes + " eyes and " + hair + " hair.");
             Console.WriteLine("His teeth are usually " + teeth + " depending on the coffee.");
 
             Console.WriteLine("If I add " + age + ", " 
-                + String.Format("{0:0.0#}",h_cms) + ", and " 
-                + String.Format("{0:0.0#}",w_kgs)
-                + " I get " + String.Format("{0:0.0#}",age + h_cms + w_kgs) 
+                + String.Format("{0:0.0#}",coefCentimeters) + ", and " 
+                + String.Format("{0:0.0#}",weightKilograms)
+                + " I get " + String.Format("{0:0.0#}",age + heightCentimeters + weightKilograms) 
                 + ".");
 
             Console.ReadKey();
