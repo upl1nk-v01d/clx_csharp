22d83d093e74ca9490e4d400a67ef3f674ecf5ce
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-19 14:19:46 +0300

    exercise 08 (and 09) is completed

---
+++ csharp-basics/exercises/FlowOfControl/exc08/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Text.RegularExpressions;
+
+class Program{
+    static void sleep(int delay=1){
+        System.Threading.Thread.Sleep(delay);
+    }
+    static void clc(){
+        Console.Clear();
+    }
+    static void exit(int p){
+        System.Environment.Exit(p);
+    }
+
+    static void displayText(string text="\n", int tick=30, int el=1, int delay=0){
+        for(int i1=0;i1<text.Length;i1++){
+            Console.Write(text[i1]);
+            sleep(tick);
+            if(el==1 && i1 == text.Length-1){
+                Console.Write("\n");
+            }
+        }
+        sleep(delay);
+    }
+
+    static int Calculation(int n1, int n2, string op){
+        int r=0;
+
+        if(op == "+"){ r = n1 + n2; }
+        if(op == "-"){ r = n1 - n2; }
+        if(op == "*"){ r = n1 * n2; }
+        if(op == "/"){ r = n1 / n2; }
+
+        return r;
+    }
+
+    static void Main(string[] args){
+
+        clc();
+        sleep(1000);
+        displayText("-= Simple Calculator =-",0,delay:1000);
+        displayText("This program does simple calculations.",delay:500);
+
+        bool quit=false, err=false;
+        int sw=0, num1=0, num2=0;
+        string prompt;
+
+        Regex rxn = new Regex("[^0-9]");
+        Regex rxa = new Regex("[^-+*/]");
+
+        while(!quit){
+
+            if(!err){ 
+                clc(); 
+            }
+
+            err = false;
+
+            if(sw==0){
+                displayText("Please enter the first whole number: ",el:0);
+            }
+            if(sw==1){
+                displayText("Please enter the second whole number: ",el:0);
+            }
+            else if(sw==2){
+                displayText("Please enter an arithmetic operator '+' '-' '*' or '/' : ",el:0);
+            }
+            
+            prompt = Console.ReadLine()!;
+
+
+            if(sw == 2){
+                if(rxa.Matches(prompt).Count > 0){
+                    err = true;
+                    clc();
+                    displayText("Input only an operator!",delay:1000);
+                    clc();
+                }
+            } else {
+                if(rxn.Matches(prompt).Count > 0){
+                    err = true;
+                    clc();
+                    displayText("Use only numbers!",delay:1000);
+                    clc();
+                }
+            }
+
+            if(prompt.Length<=0){
+                err = true;
+                clc();
+                if(sw==2){ displayText("Please input an operator.",delay:1000); }
+                else{ displayText("Please input some numbers.",delay:1000); }
+                clc();
+            } else if(prompt.ToString().Length>1 && sw==2){
+                err = true;
+                clc();
+                displayText("Please input only one operator.",delay:1000);
+                clc();
+            } 
+            
+            if(!err) {
+                int result=0;
+                sleep(150);
+                if(sw==0){ num1 = int.Parse(prompt); }
+                if(sw==1){ num2 = int.Parse(prompt); }
+                if(sw==2){ result = Calculation(num1,num2,prompt); }
+                sw++;
+                if(sw>2){
+                    displayText($"The result is {result}");
+                    sleep(1000);
+                    displayText();
+                    displayText("Press any key to retry!");
+                    displayText("Press 'q' key to exit!");
+                    if(Console.ReadKey(true).Key.ToString() == "Q"){
+                        quit = true;
+                    } else {
+                        sw=0;
+                    }
+                }
+            }
+            
+            if(quit){
+                clc();
+                quit=true;
+                displayText("Exiting program...",delay:500);
+                clc();
+                exit(1);
+            }
+        }
+    }
+}
