6c9d28de0c6cf773b05c66e831b9395dffc746dc
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-28 13:59:01 +0300

    exercise 06 is done

---
+++ csharp-basics/exercises/ClassesAndObjects/exc06/ClassDog.cs
@@ -0,0 +1,67 @@
+﻿class Dog
+{
+    public string Name { get; set; }
+
+    public string Sex { get; set; }
+
+    public string mother { get; set; }
+
+    public string father { get; set; }
+
+    public Dog(string name, string sex)
+    {
+        Name = name;
+        Sex = sex;
+        mother = null;
+        father = null;
+    }
+
+    public bool HasSameMotherAs(Dog otherDog)
+    {
+        return true;
+    }
+}
+
+class DogsList
+{
+    private List<Dog> dogs = new List<Dog>();
+
+    public void NewDog(string name, string sex)
+    {
+        dogs.Add(new Dog(name, sex));
+        Console.WriteLine($"A {sex} dog with {name} has been born! :)");
+    }
+
+    public string ListDogs()
+    {
+        string output = "";
+
+        foreach(var dog in dogs)
+        {
+            string mother = dog.mother == null ? "unknown" : dog.mother;
+            string father = dog.father == null ? "unknown" : dog.father;
+
+            output += $"{dog.Sex} {dog.Name} with mother {mother} and father {father}\n";
+        }
+
+        return output;
+    }
+
+    public List<Dog> ReturnDogs()
+    {
+        return dogs;
+    }
+    
+    public void AddParent(string name, string mother, string father)
+    {
+        foreach(var dog in dogs)
+        {
+            if(name == dog.Name)
+            {
+                dog.mother = mother;
+                dog.father = father;
+                return;
+            }
+        }
+    }
+}
