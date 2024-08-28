09da1a14c4fc16d7b7edb44663a0b530b38c6b20
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-28 12:21:08 +0300

    exercise 05 is done

--- csharp-basics/exercises/ClassesAndObjects/EnergyDrinks/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/EnergyDrinks/Program.cs
@@ -5,27 +5,30 @@ namespace EnergyDrinks
     class Program
     {
         private const int NumberedSurveyed = 12467;
+
         private const double PurchasedEnergyDrinks = 0.14;
+
         private const double PreferCitrusDrinks = 0.64;
 
         private static void Main(string[] args)
-        {
-            //fixme
-            /*
-             Console.WriteLine("Total number of people surveyed " + NumberedSurveyed);
-             Console.WriteLine("Approximately " + energyDrinkers + " bought at least one energy drink");
-             Console.WriteLine(preferCitrus + " of those " + "prefer citrus flavored energy drinks.");
-             */
+        {            
+            int energyDrinkers = Convert.ToInt32(CalculateEnergyDrinkers(NumberedSurveyed));
+            int preferCitrus = Convert.ToInt32(CalculatePreferCitrus(energyDrinkers));
+
+            Console.Clear();
+            Console.WriteLine("Total number of people surveyed " + NumberedSurveyed);
+            Console.WriteLine("Approximately " + energyDrinkers + " bought at least one energy drink");
+            Console.WriteLine(preferCitrus + " of those " + "prefer citrus flavored energy drinks.");
         }
 
-        double CalculateEnergyDrinkers(int numberSurveyed)
+        private static double CalculateEnergyDrinkers(int numberSurveyed)
         {
-            throw new Exception("Sorry, no code written :(");
+            return NumberedSurveyed * PurchasedEnergyDrinks;
         }
 
-        double CalculatePreferCitrus(int numberSurveyed)
+        private static double CalculatePreferCitrus(int energyDrinkers)
         {
-            throw new Exception("Sorry, no code written :(");
+            return energyDrinkers * PreferCitrusDrinks;
         }
     }
 }
