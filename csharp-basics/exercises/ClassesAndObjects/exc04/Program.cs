4e1da63038ad1335be584d69f0358bfdd903c3d3
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-28 11:51:27 +0300

    exercise 04 is done

---
+++ csharp-basics/exercises/ClassesAndObjects/exc04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    private static readonly MovieStore store = new MovieStore();
+
+    static private void Main(string[] args)
+    {
+        Console.Clear();
+        store.AddMovie("Casino Royale", "Eon Productions", "PG13");
+        store.AddMovie("Exile", "None Productions", "");
+        store.AddMovie("Glass", "Buena Vista International", "PG13");
+        store.AddMovie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG13");
+        
+        Console.WriteLine("\n");
+
+        var filteredMovies = store.GetPG();
+
+        foreach(var f in filteredMovies)
+        {
+            Console.WriteLine($"Movie with PG rating is {f}");
+        }
+    }
+}
