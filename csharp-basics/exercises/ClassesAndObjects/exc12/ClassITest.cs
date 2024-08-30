520303ef20898408aceeb72dc51315bdeee70567
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-30 10:04:43 +0300

    exercise 12 not done. dunno how to deal with interfaces for now.

---
+++ csharp-basics/exercises/ClassesAndObjects/exc12/ClassITest.cs
@@ -0,0 +1,55 @@
+﻿using Testpaper;
+
+namespace Testpaper
+{
+
+    public interface ITestpaper 
+    {
+        string Subject { get; set; }
+        string[] MarkScheme { get; set; }
+        string PassMark { get; set; }
+    }
+
+    public interface IStudent
+    {
+        string[] TestsTaken { get; set; }
+
+        void TakeTest(ITestpaper paper, string[] answers)
+        {
+            this.TestsTaken[TestsTaken.Length-1] = {paper, answers};
+        }
+    }
+    
+    class Student : IStudent
+    {
+        public string[] TestsTaken { get; set; }
+
+        public Student()
+        {
+            if(this.TestsTaken == null)
+            {
+                Console.WriteLine("No tests taken");
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    class Testpaper : ITestpaper
+    {
+        public string Subject { get; set; }
+
+        public string[] MarkScheme { get; set; }
+
+        public string PassMark { get; set; }
+
+        public Testpaper(string subject, string[] markScheme, string passMark)
+        {
+            Subject = subject;
+            MarkScheme = markScheme;
+            PassMark = passMark;
+        }
+    }
+}
