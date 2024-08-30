ab7023a1c2edd6c67f3bd60c60cdf69fa38f9c7a
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-30 12:48:33 +0300

    exercise 14 not done. Non-invocable member 'WeekdayInDutch' cannot be used like a method.

---
+++ csharp-basics/exercises/ClassesAndObjects/exc14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class WeekdayInDutch()
+{
+    int Year;
+    int Month;
+    int Day;
+    public WeekdayInDutch(int year, int month, int day) : this() //why ': this()' ????
+    {
+        Day = day;
+        Month = month;
+        Year = year;
+
+        GetDate(year, month, day);
+    }
+
+    public static void GetDate(int year, int month, int day)
+    {
+        var culture = new System.Globalization.CultureInfo("nl-NL");
+        var _day = culture.DateTimeFormat.GetDayName(DateTime.Today.DayOfWeek);
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        WeekdayInDutch(1970, 9, 21);
+        WeekdayInDutch(1945, 9, 2);
+        WeekdayInDutch(2001, 9, 11);
+    }
+}
+
