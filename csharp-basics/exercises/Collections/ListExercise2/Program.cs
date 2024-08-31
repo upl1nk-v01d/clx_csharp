cd6d18627ad2f6abf4a39af98b348b1bbe0aee54
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-31 12:34:35 +0300

    list exercise 02 is completed

--- csharp-basics/exercises/Collections/ListExercise2/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercise2
 {
@@ -6,8 +7,6 @@ namespace ListExercise2
     {
         private static void Main(string[] args)
         {
-            //TODO: Write a C# program to iterate through all elements in a list.
-
             var colors = new List<string>();
             colors.Add("Red");
             colors.Add("Green");
@@ -15,13 +14,13 @@ namespace ListExercise2
             colors.Add("White");
             colors.Add("Black");
 
-            /*
-            fixme
-            for (...) 
+            Console.Clear();
+            Console.WriteLine("\nPrinting List 'colors' items...\n");
+
+            for(int i = 0; i < colors.Count; i++) 
             {
-              Console.WriteLine(element);
+                Console.WriteLine(colors[i]);
             }
-            */
         }
     }
 }
