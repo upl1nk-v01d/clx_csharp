02381197102e09e2ec44e4ca9f4a01c0f8639864
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-08-31 15:40:30 +0300

    exercise 04 is completed

--- csharp-basics/exercises/Collections/UniqueValues/Program.cs
+++ csharp-basics/exercises/Collections/UniqueValues/Program.cs
@@ -1,16 +1,70 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UniqueValues
 {
     internal class Program
     {
+        public static List<string> uniques = new List<string>();
+
+        public static void IterateUniques()
+        {
+            Console.WriteLine("\nPrinting list member 'uniques' elements\n");
+
+            foreach(var item in uniques)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+
+        public static void RepeatedPrompt()
+        {
+            string prompt = "";
+
+            Console.Write("\n\nEnter name: ");
+            prompt = Console.ReadLine()!;
+
+            while(prompt != "")
+            {
+                uniques.Add(prompt);
+
+                Console.Write("Enter name: ");
+                prompt = Console.ReadLine()!;
+            } 
+        }
+
+        public static void JoinUniques()
+        {
+            string joined = "";
+
+            foreach(var item in uniques)
+            {
+                joined += item + " ";
+            }
+
+            Console.Write($"\n\nUnique name list contains: {joined}");
+        }
+        
         static void Main(string[] args)
         {
-            //ToDo: Given a non-empty list of strings, return a list that contains only unique (non-duplicate) strings.
-            //ToDo: Example: ["abc", "xyz", "klm", "xyz", "abc", "abc", "rst"] → ["klm", "rst"]
-
+            Console.Clear();
+            
             var values = new List<string> { "Hi", "Meow", "Hello", "Meow", "Hi!", "Meow", "Hi", "Bye" };
+            
+            Console.WriteLine("\nPrinting list 'values' elements\n");            
+            values.ForEach(Console.WriteLine);
 
+            uniques = values.Distinct().ToList();            
+            
+            IterateUniques();
+            
+            RepeatedPrompt();
+
+            JoinUniques();
+
+            Console.WriteLine("\n");
         }
     }
 }
