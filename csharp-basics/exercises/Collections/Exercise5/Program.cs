a14d3a88f76f09ece062ab10e80e231859a742d8
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-09-01 15:45:33 +0300

    Exercise5 is not done. problems with logorithm.

---
+++ csharp-basics/exercises/Collections/Exercise5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static int SquareDigits(int passedNumber)
+        {
+            int squared = 0;
+
+            List<int> squaredNumbers = new List<int>();
+            int length = passedNumber.ToString().ToCharArray().ToList().Count();
+
+            for(int i = 0; i < length; i++)
+            {
+                Console.WriteLine(passedNumber);
+
+                while (passedNumber != 0)
+                {
+                    Console.WriteLine("passedNumber: " + passedNumber);
+                    int digit = passedNumber % 10;
+                    Console.WriteLine("digit: " + digit);
+                    squared += digit * digit;
+                    Console.WriteLine("squared: " + squared);
+                    passedNumber /= 10;
+                    Console.WriteLine("passedNumber: " + passedNumber + "\n");
+                }
+
+                squaredNumbers.Add(squared);
+
+                passedNumber = squared;
+                squared = 0;
+            }
+            
+            return squared;
+        }
+        
+        static bool IsHappyNumber(int number)
+        {
+            int happyNumber = SquareDigits(number);
+
+            if (happyNumber == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        
+
+        private static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("\n\nPlease enter a number to see if it is happy: ");
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            string happy = IsHappyNumber(number) == true ? "" : "not";
+
+            Console.WriteLine($"The number {number} is {happy} happy number!");            
+        }
+    }
+}
