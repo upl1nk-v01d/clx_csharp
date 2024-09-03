1ac6e97a9a2e517f98b42fb137d49c0b7912ec31
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-09-03 19:53:56 +0300

    exercise 10 us completed

---
+++ csharp-basics/exercises/Collections/exc10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void DisplayHashSetItems(HashSet<string> hashSet)
+        {
+            Console.WriteLine();
+
+            foreach(var item in hashSet)
+            {
+                Console.Write(item + "\t");
+            }
+
+            Console.WriteLine("\n");
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Clear();
+
+            HashSet<string> hashSet = new HashSet<string>();
+
+            hashSet.Add("one");                
+            hashSet.Add("two");                
+            hashSet.Add("three");                
+            hashSet.Add("four");                
+            hashSet.Add("five");       
+
+            DisplayHashSetItems(hashSet);
+
+            hashSet.Clear();
+
+            hashSet.Add("duplicate");       
+            hashSet.Add("duplicate");       
+            hashSet.Add("duplicate");       
+
+            DisplayHashSetItems(hashSet);         
+        }
+    }
+}
