706f21fa98ee097a2d876ee4fb0b471e69a7de75
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-09-03 20:05:16 +0300

    LinQ exercise 04 is completed

--- csharp-basics/exercises/Collections/UniqueValues/Program.cs
+++ csharp-basics/exercises/Collections/UniqueValues/Program.cs
@@ -19,22 +19,6 @@ namespace UniqueValues
             
         }
 
-        public static void RepeatedPrompt()
-        {
-            string prompt = "";
-
-            Console.Write("\n\nEnter name: ");
-            prompt = Console.ReadLine()!;
-
-            while(prompt != "")
-            {
-                uniques.Add(prompt);
-
-                Console.Write("Enter name: ");
-                prompt = Console.ReadLine()!;
-            } 
-        }
-
         public static void JoinUniques()
         {
             string joined = "";
@@ -60,8 +44,6 @@ namespace UniqueValues
             
             IterateUniques();
             
-            RepeatedPrompt();
-
             JoinUniques();
 
             Console.WriteLine("\n");
