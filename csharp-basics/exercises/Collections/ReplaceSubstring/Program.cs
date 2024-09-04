e8c4c9b83a9e92d5b21aec8a328ac1259dd0c8db
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-09-04 17:16:45 +0300

    linq exercise 04 is completed

--- csharp-basics/exercises/Collections/ReplaceSubstring/Program.cs
+++ csharp-basics/exercises/Collections/ReplaceSubstring/Program.cs
@@ -1,12 +1,30 @@
-﻿namespace ReplaceSubstring
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ReplaceSubstring
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var words = new[] { "near", "speak", "tonight", "weapon", "customer", "deal", "lawyer" };
-            //ToDo: Write a query that replaces 'ea' substring with astersik (*) in given list of words.
-            //ToDo: "learn", "current", "deal" →  "l*rn", "current", "d*l"
+            
+            Regex regex = new Regex("ea");
+            
+            int i = 0;
+
+            foreach(var word in words)
+            {
+                words[i] = regex.Replace(word, "*");
+                i++;
+            }
+
+            Console.Clear();
+
+            foreach(var word in words)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
