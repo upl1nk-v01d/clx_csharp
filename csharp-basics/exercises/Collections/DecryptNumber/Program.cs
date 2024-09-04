83e3ddc44b2003e9c2aa3dd8c4ad7db9503806e1
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-09-04 18:05:02 +0300

    linq exercise 02 is completed

--- csharp-basics/exercises/Collections/DecryptNumber/Program.cs
+++ csharp-basics/exercises/Collections/DecryptNumber/Program.cs
@@ -5,7 +5,41 @@ namespace DecryptNumber
 {
     internal class Program
     {
-        // look at the keyboard.
+        private static string [,] _array = { 
+            {"!", "1"},
+            {"@", "2"},
+            {"#", "3"},
+            {"$", "4"},
+            {"%", "5"},
+            {"&", "6"},
+            {"*", "7"},
+            {"(", "8"},
+            {")", "9"}
+        };
+
+        private static List<string> decryptedNumbers = new List<string>();
+
+        static void decrypt(List<string> list)
+        {
+            foreach(var item in list)
+            {
+                string decrypted = "";
+
+                foreach(char c in item)
+                {
+                    for(int i = 0; i < _array.GetLength(0); i++)
+                    {
+                        if(_array[i,0] == c.ToString())
+                        {
+                            decrypted += _array[i,1];
+                        }
+                    }
+                }
+
+                decryptedNumbers.Add(decrypted);
+            }
+        }
+
         static void Main(string[] args)
         {
             var cryptedNumbers = new List<string>
@@ -17,6 +51,24 @@ namespace DecryptNumber
                 "!)(^&(#@",
                 "!)(#&%(*@#%"
             };
+
+            Console.Clear();
+
+            Console.WriteLine("crypted numbers:\n");
+
+            foreach(var item in cryptedNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n\ndecrypted numbers:\n");
+            
+            decrypt(cryptedNumbers);
+
+            foreach(var item in decryptedNumbers)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
