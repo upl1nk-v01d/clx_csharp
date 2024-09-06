4f383c1d08b5f678e662b56a7794ecc077ab91fe
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-09-06 16:43:03 +0300

    exercise 02 is completed

--- csharp-basics/exercises/Polymorphism/MakeSounds/Program.cs
+++ csharp-basics/exercises/Polymorphism/MakeSounds/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MakeSounds
 {
     class Program
     {
+        private static List<ISound> parrots = new List<ISound>();
+        
+        private static List<ISound> radios = new List<ISound>();
+
+        private static List<ISound> fireworks = new List<ISound>();
+
+        private static void MakeParrotSounds()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                ISound parrot = new Parrot();
+                parrots.Add(parrot);
+            }
+
+            foreach(var parrot in parrots)
+            {
+                parrot.PlaySound();
+            }
+        }
+
+        private static void MakeRadioSounds()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                ISound radio = new Radio();
+                radios.Add(radio);
+            }
+
+            foreach(var radio in radios)
+            {
+                radio.PlaySound();
+            }
+        }
+
+        private static void MakeFireworksSounds()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                ISound firework = new Firework();
+                fireworks.Add(firework);
+            }
+
+            foreach(var firework in fireworks)
+            {
+                firework.PlaySound();
+            }
+        }
+
         private static void Main(string[] args)
         {
+            Console.Clear();
             
+            MakeParrotSounds();
+            MakeRadioSounds();
+            MakeFireworksSounds();
         }
     }
 }
