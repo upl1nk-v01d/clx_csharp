c0081b05f7cd3e9cc484b10b84b22d4509c4d0da
upl1nk-v01d <56597891+upl1nk-v01d@users.noreply.github.com>
2024-09-10 18:23:12 +0300

    cik paspēju, tik izdarīju.

---
+++ csharp-basics/exercises/Polymorphism/VendingMachine/ClassVendingMachine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VendingMachine
+{
+    public class VendingMachine : IVendingMachine
+    {
+        public string Manufacturer { get; set; }
+
+        public bool HasProducts { get; set; }
+
+        public Money Amount { get; set; }
+        public double Balance { get; set; }
+
+        public Product[] Products { get; set; }
+
+        public List<Product> _products = new List<Product>();
+
+        public Money ConvertToMoney(int euros, int cents)
+        {
+            Money m = new Money();
+            m.Euros = euros;
+            m.Cents = cents;
+
+            return m;
+        }
+        
+        public Money InsertCoin(Money amount)
+        {
+            Balance += amount.Euros;
+
+            return amount;
+        }
+
+        public Money ReturnMoney()
+        {
+            return Amount;
+        }
+
+        public bool AddProduct(string name, Money price, int count)
+        {
+            Product product = new Product();
+
+            product.Available = count;
+            product.Price = price; 
+            product.Name = name;
+
+            _products.Add(product);
+
+            return true;
+        }
+
+        public bool UpdateProduct(int productNumber, string name, Money? price, int amount)
+        {
+            return false;
+        }
+        
+        public VendingMachine(string manufacturer, int amountEuros, int amountCents)
+        {
+            Manufacturer = manufacturer;
+
+            Money Amount = new Money();
+
+            Amount.Euros = amountEuros;
+            Amount.Cents = amountCents;
+        }
+    }
+}
--- csharp-basics/exercises/Polymorphism/VendingMachine/Program.cs
+++ csharp-basics/exercises/Polymorphism/VendingMachine/Program.cs
@@ -6,7 +6,17 @@ namespace VendingMachine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Cant change any of given code
+            //Valid money for Vending machine 0.10, 0.20, 0.50, 1.00, 2.00 coins.
+            Console.Clear();
+
+            VendingMachine vendingMachine = new VendingMachine("Coke", 100, 50);
+
+            vendingMachine.AddProduct("CocaCola", vendingMachine.ConvertToMoney(1500, 50), 5);
+            vendingMachine.InsertCoin(vendingMachine.ConvertToMoney(2, 0));
+
+            Console.WriteLine(vendingMachine.Manufacturer);
+
         }
     }
 }
